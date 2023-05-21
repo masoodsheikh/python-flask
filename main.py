194b7cfec1ae871cb6e62e8c628e4dce6eda3f2f
masoodsheikh <33185330+masoodsheikh@users.noreply.github.com>
2023-05-21 16:46:51 +0000

    created html files, dynamic data fetched, api created

--- main.py
+++ main.py
@@ -1,12 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
 
+  {
+    'id':1,
+    'title':'Data Anyalist',
+    'location':'Lahore',
+    'salary':'Rs. 500,000/-'
+  },
+  {
+    'id':2,
+    'title':'Front End Developer',
+    'location':'Faisalabad',
+    'salary':'Rs. 400,000/-'
+  },
+  {
+    'id':3,
+    'title':'Back End Developer',
+    'location':'Islamabad',
+    'salary':'Rs. 600,000/-'
+  },
+  {
+    'id':4,
+    'title':'Full Stack Developer',
+    'location':'Karachi',
+    'salary':'Rs. 700,000/-'
+  },
+]
 @app.route("/")
 def hello_world():
-  return "<p>Hello, World!</p>"
+  return render_template('home.html', jobs=JOBS)
 
-
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+    
+    
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
