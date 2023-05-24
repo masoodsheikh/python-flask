39bd245a5f1e1b4bdcb804180e39d150de5f4a49
masoodsheikh <33185330+masoodsheikh@users.noreply.github.com>
2023-05-24 11:58:00 +0000

    more database connectivity add, search by id, location, title and location added

--- database.py
+++ database.py
@@ -18,6 +18,7 @@ def load_data_from_db_old():
 
   return (result_dicts)
 
+
 def load_data_from_db():
   # Create a connection to the database.
   with engine.connect() as conn:
@@ -32,8 +33,8 @@ def load_data_from_db():
     for row in result.all():
       result_dicts.append(row._asdict())
 
-    # Iterate over the results of the query and extract the qty and rate values.
-    # Calculate the total value and add it to each dictionary as the `total` key.
+    # Iterate over the results of the query and extract the salary  values.
+    # Calculate the annual salary value and add it to each dictionary as the `annual_salary` key.
     for row in result_dicts:
       salary = row["salary"]
       total = salary * 12
@@ -41,4 +42,37 @@ def load_data_from_db():
 
     # Return the list of dictionaries.
     return result_dicts
+
+
+# function to get job by id
+def get_job_by_id(id):
+  with engine.connect() as conn:
+    query = text("SELECT * FROM jobs WHERE id = :id")
+    result = conn.execute(query, {"id": id})
+
+  row = result.fetchone()
+
+  if row is None:
+    raise ValueError(f"Job ID {id} does not exist")
+  else:
+    return (row._asdict())
+    
+#function to search jobs by location
+def search_jobs_by_location(location):
+    with engine.connect() as conn:
+        query = text("SELECT * FROM jobs WHERE LOWER(location) = LOWER(:location)")
+        result = conn.execute(query, {"location": location})
+
+    job_list = [row._asdict() for row in result]
+    return job_list
+
+#fucntion to search jobs either location wise or title wise
+def search_jobs(title=None, location=None):
+    with engine.connect() as conn:
+        query = text("SELECT * FROM jobs WHERE LOWER(title) LIKE LOWER(:title) AND LOWER(location) LIKE LOWER(:location)")
+        result = conn.execute(query, {"title": f"%{title}%" if title else "%", "location": f"%{location}%" if location else "%"})
+
+    job_list = [row._asdict() for row in result]
+    return job_list
+
 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify
-from database import load_data_from_db
+from database import load_data_from_db, get_job_by_id, search_jobs_by_location, search_jobs
+
 
 app = Flask(__name__)
 
@@ -10,10 +11,55 @@ def hello_world():
   return render_template('home.html', jobs=jobs)
 
 
+#api for getting all jobs
 @app.route("/api/jobs")
 def list_jobs():
   jobs = load_data_from_db()
   return jsonify(jobs)
+
+
+#api for getting job by id
+@app.route("/getJobById/<id>")
+def getJobById(id):
+  try:
+    job = get_job_by_id(int(id))
+  except ValueError:
+    return jsonify({"error": "Job ID does not exist"})
+  else:
+    return jsonify(job)
+
+
+#api for getting job by location
+@app.route("/getJobsByLocation/<location>")
+def getJobsByLoction(location):
+  try:
+    jobs = search_jobs_by_location(location)
+  except ValueError:
+    return jsonify({"error": "No Jobs Found"})
+  else:
+    return jsonify(jobs)
+
+
+#api for getting job by location or title
+
+from urllib.parse import unquote_plus
+
+
+@app.route("/searchJobs/<search_query>")
+def searchJobs(search_query):
+  search_query = unquote_plus(search_query)
+  params = search_query.split("/")
+  title = params[0] if params[0] != "none" else None
+  location = params[1].replace(
+    "%20", " ") if len(params) > 1 and params[1] != "none" else ""
+
+  if not title and not location:
+    return jsonify({"error": "Please provide either a title or location."})
+
+  job_list = search_jobs(title=title, location=location)
+  return jsonify(job_list)
+
+
 
 
 if __name__ == "__main__":
